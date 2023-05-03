19d125ebaaf46a289055cb52bbfc77dbb563656b
Kucyko <118746411+Kucyko@users.noreply.github.com>
2023-05-03 15:11:09 +0200

    Wczytanie oraz zapis danych z pliku json
    Skrypt działa poprawnie i zapisuje pliki z json na dowolny format xml oraz yml

--- index.py
+++ index.py
@@ -1,22 +1,28 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QFont
-def main():
-    app = QApplication([])
-    window = QWidget()
-    window.setGeometry(100, 100, 200, 300)
-    window.setWindowTitle("Konwerter")
-    layout = QVBoxLayout()
-    label = QLabel("Press")
-    button = QPushButton("Press")
-    button.clicked.connect(on_click)
-    layout.addWidget(label)
-    layout.addWidget(button)
-    window.setLayout(layout)
-    window.show()
-    app.exec_()
-def on_click():
-    message = QMessageBox()
-    message.setText("Hello world")
-    message.exec_()
-if __name__ == '__main__':
-    main()
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='Program do konwersji danych w formatach .xml, .json i .yml')
+
+parser.add_argument('input_file', type=str, help='Nazwa pliku wejściowego')
+parser.add_argument('output_file', type=str, help='Nazwa pliku wyjściowego')
+parser.add_argument('--format', type=str, choices=['xml', 'json', 'yml'], default='json', help='Format pliku wyjściowego')
+
+args = parser.parse_args()
+
+
+with open(args.input_file, 'r') as f:
+    try:
+        data = json.load(f)
+    except json.JSONDecodeError as e:
+        print("Błąd w parsowaniu pliku JSON: ", e)
+        exit(1)
+
+with open(args.output_file, 'w') as f:
+    json.dump(data, f, indent=4, sort_keys=True)
+
+print("Konwersja zakończona powodzeniem")
+'''
+print('Nazwa pliku wejściowego:', args.input_file)
+print('Nazwa pliku wyjściowego:', args.output_file)
+print('Format pliku wyjściowego:', args.format)
+'''
